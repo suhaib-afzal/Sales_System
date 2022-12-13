a6fa2b32bf58a4ae0b108aa048d894e0def73a93
SUHAIB Afzal <sxa1556@cs.bham.ac.uk>
2022-12-13 21:18:56 +0000

    commit

---
+++ WPF_UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace Sales_System_UI.ViewModels
+{
+    public class MainViewModel
+    {
+        IWindowManager manager = new WindowManager();
+
+        SalesViewModel salesViewModel = new SalesViewModel();
+
+        DatabaseViewModel databaseViewModel = new DatabaseViewModel();
+
+        public MainViewModel()
+        {
+            salesViewModel.SalesUpdatedEvent += SalesViewModel_SalesUpdatedEvent;
+            databaseViewModel.DatabaseUpdatedEvent += DatabaseViewModel_DatabaseUpdatedEvent;
+        }
+
+        private void DatabaseViewModel_DatabaseUpdatedEvent(object? sender, EventArgs e)
+        {
+            salesViewModel.ClearEverything();
+        }
+
+        private void SalesViewModel_SalesUpdatedEvent(object? sender, EventArgs e)
+        {
+            databaseViewModel.ResetAll();
+        }
+
+        public void LaunchSales()
+        {
+            manager.ShowWindowAsync(salesViewModel, null, null);
+        }
+
+        public void LaunchDatabase()
+        {
+            manager.ShowWindowAsync(databaseViewModel, null, null);
+        }
+    }
+}
